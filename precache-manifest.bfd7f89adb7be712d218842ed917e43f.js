3b2ce017ce3f3e8c61e1a58ceaf6338d32979598
Iftakhar Hasan <hasan.km.iftakhar@gmail.com>
2020-03-13 18:24:58 +0600

    Updates

---
+++ precache-manifest.bfd7f89adb7be712d218842ed917e43f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134400b2b336cbfb3ffd0eb2a2bc9ba3",
+    "url": "/react-cocktail-database/index.html"
+  },
+  {
+    "revision": "b44e08c6df39990ac299",
+    "url": "/react-cocktail-database/static/js/2.84928336.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-cocktail-database/static/js/2.84928336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c609fd99edba73cf219",
+    "url": "/react-cocktail-database/static/js/main.63e5a8fc.chunk.js"
+  },
+  {
+    "revision": "fb82df8ed1748e43b35c",
+    "url": "/react-cocktail-database/static/js/runtime-main.3689e2fa.js"
+  },
+  {
+    "revision": "0a8638b390d58132d7f8d4c784331bb3",
+    "url": "/react-cocktail-database/static/media/home.0a8638b3.svg"
+  },
+  {
+    "revision": "5529ee2bbf02b6ffd14aa67bc93b6f64",
+    "url": "/react-cocktail-database/static/media/noCocktail.5529ee2b.svg"
+  },
+  {
+    "revision": "d185a4576670f449feaf292b636e0279",
+    "url": "/react-cocktail-database/static/media/notFound.d185a457.svg"
+  }
+]);
